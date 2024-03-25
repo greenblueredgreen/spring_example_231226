f28791d6ab4b10abd64cabda840206a86d5c5358
ahyun <kaeruruk@naver.com>
2024-03-25 17:15:05 +0900

    2024년 3월 21일

---
+++ src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
@@ -0,0 +1,10 @@
+package com.example.lesson02.bo;
+
+import org.springframework.stereotype.Service;
+
+@Service //Spring bean
+public class UsedGoodsBO {
+
+	// input: (컨트롤러부터 받아온다. 그러나 아무것도 안받아옴)
+	// output: (컨트롤러에게 준다.) ㅣList<UsedGoods>
+}
