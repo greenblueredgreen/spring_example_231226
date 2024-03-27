bc6c4916854c8cf6685a0fa4b8b5605cea2df1df
ahyun <kaeruruk@naver.com>
2024-03-27 23:04:01 +0900

    2024년 3월 26일 2타

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -17,4 +17,16 @@ public class ReviewBO {
 	public Review getReview(int id) {
 		return reviewMapper.selectReview(id);
 	}
+	
+	//input : Review
+	//output : int(성공한 행의 개수)
+	public int addReview(Review review) {
+		return reviewMapper.insertReview(review);
+	}
+	
+	//addReviewAsField(4, "국밥국밥",바다, 5.0, "피곤해요!")
+	public int addReviewAsField(int stordId, String menu, String userName, Double point, String review  ) {
+		
+		return reviewMapper.insertReviewAsField(stordId, menu, userName, point, review);
+	}
 }
--- src/main/java/com/example/lesson03/mapper/ReviewMapper.java
+++ src/main/java/com/example/lesson03/mapper/ReviewMapper.java
@@ -1,10 +1,24 @@
 package com.example.lesson03.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.lesson03.domain.Review;
 
 @Mapper
 public interface ReviewMapper {
 	public Review selectReview(int id);
+	
+	//파라미터 한개일때는 @Param 하지 않는다.
+	//Mybatis가 성공한 행의 개수 return (int)
+	public int insertReview(Review review);
+
+	
+	//@Param => 하나의 Map이 된다.
+	public int insertReviewAsField(
+			@Param("storeId") int storeId11, 
+			@Param("menu") String menu11,
+			@Param("userName") String userName11, 
+			@Param("point") Double point111, 
+			@Param("review") String review11);
 }
