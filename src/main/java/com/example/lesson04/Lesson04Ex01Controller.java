3eb4423e25dfa017120554472923c5a954cd495e
ahyun <kaeruruk@naver.com>
2024-06-19 15:12:13 +0900

    2024년 6월 18일

--- src/main/java/com/example/lesson04/Lesson04Ex01Controller.java
+++ src/main/java/com/example/lesson04/Lesson04Ex01Controller.java
@@ -14,6 +14,7 @@ import com.example.lesson04.domain.User;
 
 @RequestMapping("/lesson04/ex01")
 @Controller   //jsp 화면의 경우 @ResponseBody 사용 안함
+			// html 화면의 경우 @ResponseBody 사용 안함
 public class Lesson04Ex01Controller {
 	
 	@Autowired
@@ -22,10 +23,14 @@ public class Lesson04Ex01Controller {
 
 	//회원가입 화면 (jsp로 만든 화면 -> submit -> postMapping으로 이동)
 	//http://localhost/lesson04/ex01/add-user-view  (jsp로 보내는 것/그냥 서버 주소이다.)
-	@RequestMapping(path="/add-user-view", method = RequestMethod.GET)   //오직 get만 허용하겠다는 의미
+	@RequestMapping(path="/add-user-view", method = RequestMethod.GET)   
+	//오직 get만 허용하겠다는 의미 //method = RequestMethod.GET
 	public String addUserView() {
 		return "lesson04/addUser";  //jsp의 경로 (view의 경로)
+									//html의 경로
 	}
+	
+	//회원가입 진행 => DB저장 => 결과화면 
 	
 	
 	// 서버에 파라미터 전송, DB에 넣기 -> 결과 출력 (입력 성공 return lesson04/afterAddUser ) 
@@ -46,7 +51,7 @@ public class Lesson04Ex01Controller {
 	
 	// 최근에 가입된 한 명의 유저를 보는 화면
 	// http://localhost/lesson04/ex01/latest-user-view
-	// get 매핑으로
+	// get 매핑으로, /반드시 붙이기
 	@GetMapping("/latest-user-view")
 	public String latesetUserView(Model model) {
 		//MODEL : view화면에 데이터를 넘겨주는 객체 
